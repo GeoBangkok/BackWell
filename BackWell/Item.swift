303aab4faef258bdf85fa054a6c55440737c6aa3
GeoBangkok <42850725+GeoBangkok@users.noreply.github.com>
2026-01-17 08:03:43 -0500

    Initial Commit

---
+++ BackWell/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  BackWell
+//
+//  Created by standard on 1/17/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ BackWellTests/BackWellTests.swift
@@ -0,0 +1,17 @@
+//
+//  BackWellTests.swift
+//  BackWellTests
+//
+//  Created by standard on 1/17/26.
+//
+
+import Testing
+@testable import BackWell
+
+struct BackWellTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
